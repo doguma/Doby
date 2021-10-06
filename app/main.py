9e82df1af01bf3cf2ce30681cf77699bd2daac0a
Hannah Do <hannah.hj.do@gmail.com>
2021-10-06 15:14:58 +0900

    hello?

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='doelsevier : analyze scholarly articles')
+    return render_template('index.html')
