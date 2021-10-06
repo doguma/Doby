1b42e34601ef00525a683c7e0d6b482b8d9130f6
Hannah Do <hannah.hj.do@gmail.com>
2021-10-06 22:39:07 +0900

    simple ui set up

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, Blueprint, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+if __name__ == "__main__":
+  app.run(debug=True)
