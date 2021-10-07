e98f44ec6ef7223a6458f5142ade58e696210bdf
Hannah Do <hannah.hj.do@gmail.com>
2021-10-07 16:20:47 +0900

    environ typo fixed

--- app/selenium_proc.py
+++ app/selenium_proc.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 def start_chromedriver():
     options = webdriver.ChromeOptions()
-    options.binary_location = os.environmen.get("GOOGLE_CHROME_BIN")
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     options.add_argument('headless')
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
