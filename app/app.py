6164805056505e8ad0fe7e58771bb6463809b40e
Hannah Do <hannah.hj.do@gmail.com>
2021-10-08 17:02:31 +0900

    added database url

--- app/app.py
+++ app/app.py
@@ -1,19 +1,21 @@
 from flask import Flask, render_template
 from datetime import date
 
-import time
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# from bs4 import BeautifulSoup
+import time, os
+import psycopg2
+
 from app.selenium_proc import search, trending
 
 app = Flask(__name__)
 
 
+DATABASE_URL = os.environ['postgres://lwtngwvvvflocb:19903cd3c1362eb18aadd3dc9225827123278ca939e0df869d7852c8241bc909@ec2-44-196-8-220.compute-1.amazonaws.com:5432/dd340sme9uu5or']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+
 @app.route('/')
 def index():
-    # keyword = 'insulin'
-    # res = search(keyword)
 
     res = trending()
     today = date.today()
