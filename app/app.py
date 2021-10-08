3b2dd90e316d4a1b65033722a67f052ff0e80d00
Hannah Do <hannah.hj.do@gmail.com>
2021-10-08 17:09:45 +0900

    added database url

--- app/app.py
+++ app/app.py
@@ -9,9 +9,9 @@ from app.selenium_proc import search, trending
 app = Flask(__name__)
 
 
-DATABASE_URL = os.environ['postgres://lwtngwvvvflocb:19903cd3c1362eb18aadd3dc9225827123278ca939e0df869d7852c8241bc909@ec2-44-196-8-220.compute-1.amazonaws.com:5432/dd340sme9uu5or']
+# DATABASE_URL = os.environ['postgres://lwtngwvvvflocb:19903cd3c1362eb18aadd3dc9225827123278ca939e0df869d7852c8241bc909@ec2-44-196-8-220.compute-1.amazonaws.com:5432/dd340sme9uu5or']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 @app.route('/')
