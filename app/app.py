c8ee40b214e18bed541043e1a97983dbcf0b4ef9
Hannah Do <hannah.hj.do@gmail.com>
2021-10-11 01:30:34 +0900

    gohome func name change

--- app/app.py
+++ app/app.py
@@ -203,8 +203,9 @@ def search():
 
         return render_template("search.html", search_articles = articles, keywords = keywords, ngram1 = ngram1_s, ngram2 = ngram2_s, ngram3 = ngram3_s)
 
+
 @app.route("/go-home", methods=["GET", "POST"])
-def delete():
+def gohome():
     if request.form:
         request.form.get("go-home")
 
