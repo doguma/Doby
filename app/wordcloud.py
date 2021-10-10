f639a7ecbc70b2d95d56f3e4a1f3cae083fbf4ef
Hannah Do <hannah.hj.do@gmail.com>
2021-10-10 19:31:58 +0900

    concatenate string

--- app/wordcloud.py
+++ app/wordcloud.py
@@ -18,14 +18,38 @@ def createcloud(res):
 
     total_tokens = word_tokenize(collection)
     total_tokens2 = []
+    total_tokens3 = []
     for word in total_tokens:
         word = re.sub('[^a-zA-Z0-9_-]', '', word).strip()
         word = word.lower()
         if word not in stopwords.words('english') and len(word)>1 and word not in blank_words:
             total_tokens2.append(word)
 
+    for word in total_tokens:
+        word = re.sub('[^a-zA-Z0-9_-]', '', word).strip()
+        word = word.lower()
+        if word not in stopwords.words('english') and len(word)>1:
+            total_tokens2.append(word)
+
     counted_1 = Counter(total_tokens2).most_common()[:15]
     counted_2 = Counter(ngrams(total_tokens2, 2)).most_common()[:15]
+
+    bigrams = []
+
+    for i, j in counted_2:
+        temp2 = []
+        temp2.append(' '.join(i))
+        temp2.append(j)
+        bigrams.append(temp2)
+
     counted_3 = Counter(ngrams(total_tokens2, 3)).most_common()[:15]
 
-    return dict(counted_1), dict(counted_2), dict(counted_3)
+    trigrams = []
+
+    for i, j in counted_2:
+        temp2 = []
+        temp2.append(' '.join(i))
+        temp2.append(j)
+        trigrams.append(temp2)
+
+    return dict(counted_1), dict(bigrams), dict(trigrams)
