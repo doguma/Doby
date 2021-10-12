82c4020ec5387d7bc80754e0dc08cf7edc8baac9
Hannah Do <hannah.hj.do@gmail.com>
2021-10-12 17:51:01 +0900

    AS

--- app/app.py
+++ app/app.py
@@ -236,7 +236,7 @@ def search():
 
         # return render_template("search.html", search_articles = articles, keywords = keywords, ngram1 = ngram1_s, ngram2 = ngram2_s, ngram3 = ngram3_s, random_sentence = thesis)
 
-    return render_template("search.html", search_articles = articles, keywords = keywords, ngram1 = ngram1_s, ngram2 = ngram2_s, ngram3 = ngram3_s, random_sentence = thesis)
+        return render_template("search.html", search_articles = articles, keywords = keywords, ngram1 = ngram1_s, ngram2 = ngram2_s, ngram3 = ngram3_s, random_sentence = thesis)
 
 
 
