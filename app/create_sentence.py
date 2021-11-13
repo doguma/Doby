bfacd8f214f8a3c30c57e8803e9129180352ad48
Hannah Do <hannah.hj.do@gmail.com>
2021-11-13 21:09:16 +0900

    adding class inside

--- app/create_sentence.py
+++ app/create_sentence.py
@@ -7,16 +7,18 @@ warnings.filterwarnings('ignore')
 # code here referenced from :
 # https://towardsdatascience.com/text-generation-with-markov-chains-an-introduction-to-using-markovify-742e6680dc33
 
-nlp = en_core_web_sm.load()
 
-class POSifiedText(markovify.Text):
-    def word_split(self, sentence):
-        return ['::'.join((word.orth_, word.pos_)) for word in nlp(sentence)]
-    def word_join(self, words):
-        sentence = ' '.join(word.split('::')[0] for word in words)
-        return sentence
 
 def random_sentence(collection):
+
+    nlp = en_core_web_sm.load()
+
+    class POSifiedText(markovify.Text):
+        def word_split(self, sentence):
+            return ['::'.join((word.orth_, word.pos_)) for word in nlp(sentence)]
+        def word_join(self, words):
+            sentence = ' '.join(word.split('::')[0] for word in words)
+            return sentence
 
     gen2 = POSifiedText(collection, state_size=1)
 
